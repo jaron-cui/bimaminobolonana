b7cd15324b62e34adb8015d03a3452df9d12ee7f
jaroncui <jaron.cui@gmail.com>
2025-10-20 13:12:04 -0400

    basic logging and trainer class prototype

---
+++ robot/train_utils.py
@@ -0,0 +1,86 @@
+from dataclasses import dataclass
+from datetime import datetime
+import glob
+from typing import List, cast
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+
+
+@dataclass
+class Job:
+  """
+  logs/
+    date_tag/
+      debug.log
+      checkpoint/
+        <training-stage-name1>/
+          0.pt
+          ...
+          n.pt
+        <training-stage-name2>/
+          ...
+      artifact/
+        ...
+  """
+  path: Path
+  date: datetime
+  tag: str
+  
+  @property
+  def debug_log(self) -> Path:
+    return self.path / 'debug.log'
+
+  def load_checkpoint(self, training_stage: str, epoch: int) -> nn.Module:
+    return torch.load(self._checkpoint_path(training_stage, epoch))
+  
+  def save_checkpoint(self, model: nn.Module, training_stage: str, epoch: int) -> Path:
+    checkpoint_path = self._checkpoint_path(training_stage, epoch)
+    torch.save(model, checkpoint_path)
+    return checkpoint_path
+
+  def _checkpoint_path(self, training_stage: str, epoch: int) -> Path:
+    return self.path / f'checkpoint/{training_stage}/{epoch}.pt'
+  
+  def __str__(self) -> str:
+    return f'Job({self.path.name})'
+
+
+class Logs:
+  def __init__(self, path: str):
+    self.root_dir = Path(path)
+    if not self.root_dir.exists():
+      raise ValueError('Path {path} does not exist. Provide the absolute path to your training output directory.')
+    
+  def jobs(self) -> List[Job]:
+    jobs = []
+    for path in glob.glob('*', root_dir=self.root_dir):
+      job = Logs._parse_job_path(Path(path), silent_error=True)
+      if job is None:
+        continue
+      jobs.append(job)
+    return jobs
+  
+  def create_new_job(self, tag: str) -> Job:
+    date = datetime.now()
+    path = self.root_dir / f'{date.isoformat()}_{tag}'
+    if path.exists():
+      raise ValueError(f'Job at {path} already exists!')
+    path.mkdir()
+    return cast(Job, self._parse_job_path(path))
+
+  @staticmethod
+  def _parse_job_path(job_path: Path, silent_error: bool = False):
+    parts = '_'.split(job_path.name)
+    parsers = [datetime.fromisoformat, lambda t: t]
+    if len(parts) < len(parsers):
+      if silent_error:
+        return None
+      raise ValueError('Invalid job name.')
+    parsed_parts = [parse(part) for part, parse in zip(parts, parsers)]
+    return Job(
+      path=job_path,
+      date=parsed_parts[0],
+      tag=parsed_parts[1]
+    )
---
+++ robot/trainer.py
@@ -0,0 +1,78 @@
+import abc
+from typing import Callable
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from robot.sim import BimanualObs, BimanualSim
+from robot.train_utils import Job, Logs
+
+
+class BimanualActor(nn.Module, abc.ABC):
+  @abc.abstractmethod
+  def forward(self, obs: BimanualObs) -> Tensor:
+    raise NotImplementedError()
+
+
+class BCTrainer:
+  def __init__(
+    self,
+    dataloader,
+    checkpoint_frequency: int,
+    job: Job
+  ):
+    self.dataloader = dataloader
+    self.checkpoint_frequency = checkpoint_frequency
+    self.job = job
+  
+  def train(self, model: BimanualActor, num_epochs: int):
+    # TODO: move much of the details here into config
+    optimizer = torch.optim.Adam(model.parameters())
+    criterion = nn.MSELoss()
+    for epoch in range(num_epochs):
+      for obs, action in self.dataloader:
+        predicted_action = model(obs)
+        loss = criterion(predicted_action, action)
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        optimizer.step()
+
+      if epoch % self.checkpoint_frequency == 0:
+        self.job.save_checkpoint(model, 'bc-pretrain', epoch)
+
+
+class RLTrainer:
+  def __init__(
+    self,
+    dataloader,
+    checkpoint_frequency: int,
+    job: Job,
+    rollouts_per_epoch: int,
+    rollout_step_count: int,
+    init_sim: Callable[[], BimanualSim] = BimanualSim
+  ):
+    self.dataloader = dataloader
+    self.checkpoint_frequency = checkpoint_frequency
+    self.job = job
+    self.rollouts_per_epoch = rollouts_per_epoch
+    self.rollout_step_count = rollout_step_count
+    self.init_sim = init_sim
+
+  def train(self, model: BimanualActor, num_epochs: int):
+    sim = self.init_sim()
+    # TODO: implement RL. perhaps PPO?
+    for epoch in range(num_epochs):
+      for rollout_number in range(self.rollouts_per_epoch):
+        obs = sim.get_obs()
+        for rollout_step_number in range(self.rollout_step_count):
+          predicted_action = model(obs)
+          obs = sim.step(predicted_action)
+          # TODO: RL reward collection / rollout buffer updates?
+        # TODO: RL weight updates
+
+      if epoch % self.checkpoint_frequency == 0:
+        self.job.save_checkpoint(model, 'rl-train', epoch)
+
