84ed97088c4c444db81a4695942e769afd1f723b
jaroncui <jaron.cui@gmail.com>
2025-10-24 12:52:08 -0400

    moving bc dataset file logic into helper class

--- train/generate_bc_data.py
+++ train/generate_bc_data.py
@@ -1,12 +1,145 @@
+from dataclasses import dataclass, field
 import os
 from pathlib import Path
 from typing import List, Tuple
 
 import numpy as np
+import torch
 from tqdm import tqdm
 
 from policy.privileged_policy import PrivilegedPolicy
 from robot.sim import BimanualAction, BimanualObs, BimanualSim, JOINT_OBSERVATION_SIZE, ACTION_SIZE, randomize_block_position
+
+
+class BimanualDataset(torch.utils.data.Dataset):
+  def __init__(self, data_directory: Path | str):
+    super().__init__()
+    self._observation_array = None
+
+
+@dataclass
+class BimanualDatasetMetadata:
+  save_dir: Path
+  total_sample_count: int
+  max_steps_per_rollout: int
+  camera_height: int
+  camera_width: int
+  skip_frames: int
+  sample_count: int = 0
+
+  rollout_lengths: List[int] = field(default_factory=list)
+
+  @property
+  def rollout_count(self) -> int:
+    return len(self.rollout_lengths)
+
+  @property
+  def metadata_file_path(self) -> Path:
+    return self.save_dir / 'metadata.txt'
+  
+  @property
+  def observation_file_path(self) -> Path:
+    return self.save_dir / 'observations.npy'
+  
+  @property
+  def action_file_path(self) -> Path:
+    return self.save_dir / 'actions.npy'
+  
+  @property
+  def rollout_length_file_path(self) -> Path:
+    return self.save_dir / 'rollout_length.npy'
+  
+  @property
+  def observation_size(self) -> int:
+    return 2 * (self.camera_height * self.camera_width * 3) + JOINT_OBSERVATION_SIZE + JOINT_OBSERVATION_SIZE
+  
+  @property
+  def action_size(self) -> int:
+    return ACTION_SIZE
+  
+  @property
+  def size_in_gigabytes(self) -> float:
+    return self.total_sample_count * (self.observation_size + ACTION_SIZE) * 4 / 1e9
+  
+  def memmap_data(
+    self,
+  ) -> Tuple[np.ndarray, np.ndarray] | None:
+    existing_metadata = BimanualDatasetMetadata.from_file(self.save_dir)
+    if existing_metadata is not None:
+      for field, current, existing in [
+        ('total_sample_count', self.total_sample_count, existing_metadata.total_sample_count),
+        ('max_steps_per_rollout', self.max_steps_per_rollout, existing_metadata.max_steps_per_rollout),
+        ('camera_height', self.camera_height, existing_metadata.camera_height),
+        ('camera_width', self.camera_width, existing_metadata.camera_width),
+        ('skip_frames', self.skip_frames, existing_metadata.skip_frames)
+      ]:
+        if current != existing:
+          raise ValueError(f'Bimanual dataset metadata values don\'t match: {field}=={existing}!={current}')
+        
+    if any([not path.exists() for path in [
+      self.metadata_file_path,
+      self.observation_file_path,
+      self.action_file_path,
+      self.rollout_length_file_path
+    ]]):
+      response = input(
+        f'This will allocate {self.size_in_gigabytes:.2f}GB in `{self.save_dir}`. '
+        'Are you sure you want to proceed? (y/[n])'
+      )
+      if response != 'y':
+        print('Canceled.')
+        return None
+      print(f'Allocating {self.size_in_gigabytes:.2f}GB in `{self.save_dir}`...')
+      self.sample_count = 0
+      self.rollout_lengths = []
+      os.makedirs(self.save_dir, exist_ok=True)
+      np.save(self.observation_file_path, np.zeros((self.total_sample_count, self.observation_size), dtype=np.float32))
+      np.save(self.action_file_path, np.zeros((self.total_sample_count, self.action_size), dtype=np.float32))
+      np.save(self.rollout_length_file_path, np.array(self.rollout_lengths, dtype=np.uint16))
+      self.update_data_pointers()
+
+    observation_array = np.memmap(
+      self.observation_file_path, dtype=np.float32, mode='r+', shape=(self.total_sample_count, self.observation_size))
+    action_array = np.memmap(
+      self.action_file_path, dtype=np.float32, mode='r+', shape=(self.total_sample_count, self.action_size))
+    return observation_array, action_array
+  
+  def update_data_pointers(self, new_rollout_length: int | None = None):
+    if new_rollout_length:
+      self.sample_count += new_rollout_length
+      self.rollout_lengths.append(new_rollout_length)
+    metadata_values = {
+      'total_sample_count': self.total_sample_count,
+      'max_steps_per_rollout': self.max_steps_per_rollout,
+      'camera_height': self.camera_height,
+      'camera_width': self.camera_width,
+      'skip_frames': self.skip_frames,
+      'sample_count': self.sample_count,
+      'rollout_count': self.rollout_count
+    }
+    with open(self.metadata_file_path, 'w') as file:
+      file.write(' '.join([f'{field}={value}' for field, value in metadata_values.items()]))
+
+  @staticmethod
+  def from_file(save_dir: Path | str) -> 'BimanualDatasetMetadata | None':
+    metadata_file_path = Path(save_dir) / 'metadata.txt'
+    rollout_length_file_path = Path(save_dir) / 'rollout_length.npy'
+    if not metadata_file_path.exists() or not rollout_length_file_path:
+      return None
+    
+    with open(metadata_file_path, 'r') as file:
+      parts = file.read().split(' ')
+
+    return BimanualDatasetMetadata(
+      save_dir=Path(save_dir),
+      total_sample_count=int(parts[0][len('total_sample_count='):]),
+      max_steps_per_rollout=int(parts[1][len('max_steps_per_rollout='):]),
+      camera_height=int(parts[2][len('camera_height='):]),
+      camera_width=int(parts[3][len('camera_width='):]),
+      skip_frames=int(parts[4][len('skip_frames='):]),
+      sample_count=int(parts[5][len('sample_count='):]),
+      rollout_lengths=np.load(rollout_length_file_path)[:int(parts[6][len('rollout_count='):])]
+    )
 
 
 def record_bc_data(
